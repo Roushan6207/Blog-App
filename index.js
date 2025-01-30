4dbd3cc087899a51ff7926345eb60d50316fdb74
Roushan Kumar <your_email@example.com>
2025-01-31 02:11:23 +0530

    going on fire

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require("express");
-const app=express;
+const app=express();
 
 app.listen(3000, () =>{
     console.log("App is going Good");
+})
+
+app.get("/",(req,res) => {
+    console.log("This is HomePage baby");
+    res.send(`<h1>welcome to HomePage</h1>`);
 })
