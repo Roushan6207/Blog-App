ea87e3bd7e74fa9e8f46e03ed7618b18be76ea96
Roushan Kumar <your_email@example.com>
2025-01-31 02:55:18 +0530

    1st day done

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express=require("express");
-const app=express();
 
-app.listen(3000, () =>{
-    console.log("App is going Good");
-})
+const express = require("express");
+ const app= express();
 
-app.get("/",(req,res) => {
-    console.log("This is HomePage baby");
-    res.send(`<h1>welcome to HomePage</h1>`);
-})
+ require("dotenv").config();
+ const PORT= process.env.PORT || 3000;
+
+ //middleware
+ app.use(express.json());
+ 
+ const blog = require("./routes/blog");
+
+ //mount 
+ app.use("/api/v1",blog);
+
+ const connectWithDb = require("./config/database");
+ connectWithDb();
+
+ //start the server
+ app.listen( PORT, () => {
+    console.log(`App is  running at port no ${PORT}`);
+ })
+
+ app.get("/", (req,res) => {
+    res.send(`<h1> This is Homepage babay </h1>`)
+ })
---
+++ routes/blog.js
@@ -0,0 +1,9 @@
+const express= require("express");
+const router = express.Router();
+
+//import controller...
+
+// mapping create of  router...
+
+
+//export the router...
