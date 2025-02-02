56499601e79118bce5c7c01bed09ca21c605bf5a
Roushan Kumar <your_email@example.com>
2025-02-03 01:53:47 +0530

    upto commentControllers

--- routes/blog.js
+++ routes/blog.js
@@ -2,8 +2,13 @@ const express= require("express");
 const router = express.Router();
 
 //import controller...
+const {createComment}=require("../controllers/CommentController");
+
+
 
 // mapping create of  router...
+router.post("/comments/create",createComment);
 
 
 //export the router...
+module.exports=router;
